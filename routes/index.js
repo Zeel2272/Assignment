792f02bd32081856d8d351401dfec21b185afddb
Zeel Thakkar <zeelthakkar2272@gmail.com>
2021-11-23 00:27:42 -0400

    Error page created

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.get('/feedback', function(req, res, next) {
+  res.render('feedback');
+});
+
+
 
 module.exports = router;
